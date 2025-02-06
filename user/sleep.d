05e95a755d19dd396a4ca1a97674a7b0215ff3dc
Teknique <martin.wright@resideo.com>
2025-02-07 11:19:42 +1300

    Cleaned makefiles

--- user/sleep.d
+++
@@ -1,2 +0,0 @@
-user/sleep.o: user/sleep.c /usr/riscv64-linux-gnu/include/stdc-predef.h \
- kernel/types.h user/user.h
--- user/wlan_cfg.d
+++
@@ -1,4 +0,0 @@
-user/wlan_cfg.o: user/wlan_cfg.c \
- /usr/riscv64-linux-gnu/include/stdc-predef.h kernel/types.h \
- kernel/stat.h user/user.h \
- /usr/lib/gcc-cross/riscv64-linux-gnu/11/include/stddef.h
